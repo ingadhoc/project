52cf2ccd63b0796f8a5790a7180972f0b3bf565d
susport <richard.pompa84@gmail.com>
2016-08-01 15:24:33 -0300

    9.0-mig-project_category
    This module add the category type fields in project.

--- project_category/__openerp__.py
+++ project_category/__openerp__.py
@@ -24,10 +24,6 @@
     'category': 'Projects & Services',
     'sequence': 14,
     'summary': '',
-    'description': """
-Project Category
-=====================
-    """,
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
@@ -45,7 +41,7 @@ Project Category
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
