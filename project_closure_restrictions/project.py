cd36ede38d3e48b925e9f43873c267cdb8f4cfe8
susport <richard.pompa84@gmail.com>
2016-08-02 11:59:19 -0300

    9.0-mig-project_closure_restrictions
    Validando a atrvés de @api.constrains

--- project_closure_restrictions/project.py
+++ project_closure_restrictions/project.py
@@ -10,8 +10,9 @@ from openerp.exceptions import Warning
 class project_project(models.Model):
     _inherit = 'project.project'
 
-    @api.onchange('state')
-    def onchange_state(self):
+    @api.one
+    @api.constrains('state')
+    def validate_state(self):
         if self.state == 'close':
             for project in self:
                 if project.task_ids:
@@ -23,18 +24,3 @@ class project_project(models.Model):
                             _("You can not close a project with active task,"
                               " we consider active task the one in stages "
                               "without option 'folded'"))
-
-    @api.multi
-    def write(self, vals):
-        if vals.get('state') == 'close':
-            for project in self:
-                if project.task_ids:
-                    tasks_open = project.env['project.task'].search(
-                        [('id', 'in', [x.id for x in project.task_ids]),
-                         ('stage_id.fold', '!=', True)])
-                    if tasks_open:
-                        raise Warning(
-                            _("You can not close a project with active task,"
-                              " we consider active task the one in stages "
-                              "without option 'folded'"))
-        return super(project_project, self).write(vals)
