0d0b467575ab5f624c1a5eb832446eff5fde82d1
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-12-19 18:24:33 -0300

    Revert "DEL project_task_portal_unfollow/"
    This reverts commit 7a70ef282db75127f17097c821184015ceef677c.

---
+++ project_task_portal_unfollow/project.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from openerp import models, api, fields
+
+
+class project(models.Model):
+    _inherit = 'project.project'
+
+    unfollow_portal_users = fields.Boolean(
+        'Unfollow Portal Users',
+        help='Unfollow Portal Users from new tasks?'
+    )
+
+
+class project_task(models.Model):
+    _inherit = 'project.task'
+
+    @api.model
+    def create(self, vals):
+        task = super(project_task, self).create(vals)
+        if not task.project_id.unfollow_portal_users:
+            return task
+        partner_to_unfollow_ids = []
+        for follower in task.message_partner_ids:
+            if follower.user_ids:
+                for user in follower.user_ids:
+                    if not user.sudo(user).user_has_groups('base.group_user'):
+                        partner_to_unfollow_ids.append(follower.id)
+                        continue
+            else:
+                partner_to_unfollow_ids.append(follower.id)
+        task.message_unsubscribe(partner_to_unfollow_ids)
+        return task
+
+    @api.one
+    def write(self, vals):
+        res = super(project_task, self).write(vals)
+        if not self.project_id.unfollow_portal_users:
+            return res
+        partner_to_unfollow_ids = []
+        for follower in self.message_partner_ids:
+            if follower.user_ids:
+                for user in follower.user_ids:
+                    if not user.sudo(user).user_has_groups('base.group_user'):
+                        partner_to_unfollow_ids.append(follower.id)
+                        continue
+            else:
+                partner_to_unfollow_ids.append(follower.id)
+        self.message_unsubscribe(partner_to_unfollow_ids)
+        return res
