d93f7c0f0dcb7e2fd97b429b5ffc75efaf921c2a
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-01-20 17:18:02 -0300

    FIX

--- project_closure_restrictions/project.py
+++ project_closure_restrictions/project.py
@@ -14,15 +14,14 @@ class ProjectProject(models.Model):
     @api.one
     @api.constrains('state')
     def validate_state(self):
-        # tests are boken by this check
-        if self.analytic_account_id.account_type != 'closed' and not \
-                config.options['test_enable'] and \
-                config.options['without_demo']:
-            raise ValidationError(_(
-                "You can not cancel the project if the analytic account is not"
-                "closed"))
-
         if self.state == 'close':
+            # tests are boken by this check
+            if self.analytic_account_id.account_type != 'closed' and not \
+                    config.options['test_enable'] and \
+                    config.options['without_demo']:
+                raise ValidationError(_(
+                    "You can not cancel the project if the analytic account is"
+                    " not closed"))
             for project in self:
                 if project.task_ids:
                     tasks_open = project.env['project.task'].search(
