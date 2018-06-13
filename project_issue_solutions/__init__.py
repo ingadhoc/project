9593aeeed577dee8d94b61756bbd2a90c94eeea0
Katherine Zaoral <zaoral@users.noreply.github.com>
2018-06-13 11:24:16 -0300

    [REL] project_issue_solutions: Migration version 11.0 (#61)
    This one was moved to enterprise-extensions and now works with helpdesk.ticket

--- project_issue_solutions/__init__.py
+++
@@ -1,9 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-
-from . import project_issue
-from . import project_solution_tags
-from . import project_issue_solution
-from . import project_task_type
