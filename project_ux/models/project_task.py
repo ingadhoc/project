d8fbcf582b8bd313a2869bf4fa5a0e18677bdfbc
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-08-06 18:08:34 -0300

    [FIX] fix the subtasks count and the some fixes (#68)

--- project_ux/models/project_task.py
+++ project_ux/models/project_task.py
@@ -35,3 +35,20 @@ class ProjectTask(models.Model):
                      'company_id', 'message_last_post']:
                 continue
             self.update({k: v})
+
+    # We overwrite this function because it is not possible to inherit it and
+    # we do that calculates only the subtasks that are in the closed stages.
+    @api.multi
+    def _compute_subtask_count(self):
+        for task in self:
+            task.subtask_count = self.search_count(
+                [('id', 'child_of', task.id),
+                 ('id', '!=', task.id),
+                 '|', ('stage_id.fold', '=', False),
+                 ('stage_id', '=', False)])
+
+    def action_subtask(self):
+        action = super(ProjectTask, self).action_subtask()
+        if action.get('context', {}).get('search_default_is_task', False):
+            action.get('context').pop('search_default_is_task')
+        return action
