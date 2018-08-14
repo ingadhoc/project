ab3a553a792753452ec28274b1c609f2b572eea6
nicomacr <nmr@adhoc.com.ar>
2018-08-14 09:43:26 -0300

    [IMP] improve the duplicate project with subtask

--- project_ux/models/project_project.py
+++ project_ux/models/project_project.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ProjectProject(models.Model):
@@ -36,3 +36,29 @@ class ProjectProject(models.Model):
                       for data in task_data)
         for project in self:
             project.update({'task_count': result.get(project.id, 0)})
+
+    @api.multi
+    def copy(self, default=None):
+        tasks = self.task_ids
+        project = super(ProjectProject, self).copy(default)
+        # We setting the if the subtask project and the project are the same
+        # the new id of the subtask project and the new project
+        #  for the new tasks
+        if self.subtask_project_id == self:
+            (self.tasks - tasks).update({'project_id': project.id})
+            project.subtask_project_id = project.id
+        return project
+
+    @api.multi
+    def map_tasks(self, new_project_id):
+        """ copy and map tasks from old to new project """
+        tasks = self.env['project.task']
+        # We filtered by the tasks only, the subtask will duplicate by the
+        # parent
+        for task in self.tasks.filtered(lambda x: not x.parent_id):
+            # preserve task name and stage, normally altered during copy
+            defaults = {'stage_id': task.stage_id.id,
+                        'name': task.name}
+            tasks += task.copy(defaults)
+        return self.browse(new_project_id).write(
+            {'tasks': [(6, 0, tasks.ids)]})
