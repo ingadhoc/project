817134481b2b00cc770b86e1054e50d026fa35e2
nicomacr <nmr@adhoc.com.ar>
2018-08-17 10:34:09 -0300

    [FIX] fix inherit less o_kanban_dashboard_project

--- project_stage/__manifest__.py
+++ project_stage/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Project Stage',
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.2.0',
     'category': 'Projects & Services',
     'sequence': 14,
     'summary': '',
