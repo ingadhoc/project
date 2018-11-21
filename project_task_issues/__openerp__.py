9acf8d0114e7eec0d4491c3cc998cbd481471a83
nicomacr <nmr@adhoc.com.ar>
2018-11-21 15:40:24 -0300

    [FIX] change the priority to the tasks view to avoid an error related with some field in the view

--- project_task_issues/__openerp__.py
+++ project_task_issues/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Project Task Issues",
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.1.0',
     'category': 'Project',
     'sequence': 14,
     'summary': '',
