172f6041974735aac862dae317f5318ac807ab6d
Lucas L. Soto <ls@adhoc.com.ar>
2019-07-16 11:43:39 -0300

    [MIG] Bump to 12.0, make modules uninstallable and deprecate some modules

--- project_related_projects/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import project
-from . import wizard
-from . import report
