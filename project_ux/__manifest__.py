8dfac6c63194effcf55df46e13e3db523bf27438
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-11-15 07:20:31 -0300

    [FIX] project_ux: stages always visible

--- project_ux/__manifest__.py
+++ project_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Project UX',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.1.0',
     'category': 'Project Management',
     'sequence': 14,
     'author': 'ADHOC SA',
