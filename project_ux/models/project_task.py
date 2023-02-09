c3f9cb672e5aab6e5b1422a8bf0812bd281c63ff
Virginia Bonservizi <vib@adhoc.com.ar>
2023-02-09 15:34:21 -0300

    [IMP] project_ux: change to send the stage change message to task followers
    X-original-commit: 31b0c4a41dce975feb131cb8f529e87a08376292

--- project_ux/models/project_task.py
+++ project_ux/models/project_task.py
@@ -19,6 +19,11 @@ class Task(models.Model):
     def _track_template(self, changes):
         task = self[0]
         res = super()._track_template(changes)
+        if 'stage_id' in changes and task.stage_id.mail_template_id:
+            res['stage_id'] = (task.stage_id.mail_template_id, {
+                'message_type': 'comment',
+                'auto_delete_message': True,
+                'email_layout_xmlid': 'mail.mail_notification_light'})
         if 'stage_id' in res and task.dont_send_stage_email and task.stage_id.mail_template_id:
             res.pop('stage_id')
             task.dont_send_stage_email = False
