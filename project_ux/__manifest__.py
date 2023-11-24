294fa47bfb9e09c575cedc4af69cf9660561cbb2
Juan Ignacio Carreras <jc@adhoc.com.ar>
2023-11-24 15:28:13 +0000

    [ADD]project_ux: planned_date_begin visible
    closes ingadhoc/project#114
    
    Signed-off-by: Bruno Zanotti <bz@adhoc.com.ar>

--- project_ux/__manifest__.py
+++ project_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Project UX',
-    'version': "16.0.1.1.0",
+    'version': "16.0.1.2.0",
     'category': 'Project Management',
     'sequence': 14,
     'author': 'ADHOC SA',
