e66c64fe0841fbbd9708a66ec3baf3fc9b22d8ba
mav-adhoc <mav@adhoc.com.ar>
2024-07-31 13:22:25 -0300

    [FIX] project_ux: restriction on task_state if state_change boolean is selected

--- project_ux/models/project_task_type.py
+++ project_ux/models/project_task_type.py
@@ -2,7 +2,8 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 class ProjectTaskType(models.Model):
     _inherit = 'project.task.type'
@@ -13,3 +14,9 @@ class ProjectTaskType(models.Model):
     )
 
     task_state = fields.Selection(selection=lambda self: self.env['project.task'].fields_get(allfields=['state'])['state']['selection'])
+
+    @api.constrains('state_change', 'task_state')
+    def _check_task_state(self):
+        for record in self:
+            if record.state_change and not record.task_state:
+                raise UserError("The state to which the task should change cannot be empty if 'state change' is checked")
