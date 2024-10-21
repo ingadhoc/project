22eb970d7b040a5ed772cd2af195b0883a71beb2
docker-odoo <docker-odoo@example.com>
2024-10-21 13:02:03 +0000

    [MIG] project_ux: Migration to 18.0

--- project_ux/__manifest__.py
+++ project_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Project UX',
-    'version': "17.0.1.2.0",
+    'version': "18.0.1.0.0",
     'category': 'Project Management',
     'sequence': 14,
     'author': 'ADHOC SA',
@@ -38,7 +38,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
