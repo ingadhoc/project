eb2bd7025ef31a027dee541020301f66ddb49764
mav-adhoc <mav@adhoc.com.ar>
2025-02-05 11:49:27 -0300

    [18.0][ADD] pre-commit

--- project_my_tasks_by_stage/__manifest__.py
+++ project_my_tasks_by_stage/__manifest__.py
@@ -1,21 +1,20 @@
 {
-    'name': 'Project My Tasks By Stage',
-    'version': "18.0.1.0.0",
-    'category': 'Services/Project',
-    'sequence': 50,
-    'summary': '',
-    'author': 'ADHOC SA',
-    'website': 'www.adhoc.com.ar',
-    'license': 'AGPL-3',
-    'images': [
+    "name": "Project My Tasks By Stage",
+    "version": "18.0.1.0.0",
+    "category": "Services/Project",
+    "sequence": 50,
+    "summary": "",
+    "author": "ADHOC SA",
+    "website": "www.adhoc.com.ar",
+    "license": "AGPL-3",
+    "images": [],
+    "depends": [
+        "project",
     ],
-    'depends': [
-        'project',
+    "data": [
+        "views/project_task_views.xml",
     ],
-    'data': [
-        'views/project_task_views.xml',
-    ],
-    'installable': True,
-    'auto_install': False,
-    'application': False,
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
--- project_ux/models/project_task_type.py
+++ project_ux/models/project_task_type.py
@@ -2,21 +2,26 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields, api
+from odoo import api, fields, models
 from odoo.exceptions import UserError
 
+
 class ProjectTaskType(models.Model):
-    _inherit = 'project.task.type'
+    _inherit = "project.task.type"
 
     state_change = fields.Boolean(
         default=False,
-        help="When the task's stage changes, if the state change is checked it will change the state of the task into the state selected"
+        help="When the task's stage changes, if the state change is checked it will change the state of the task into the state selected",
     )
 
-    task_state = fields.Selection(selection=lambda self: self.env['project.task'].fields_get(allfields=['state'])['state']['selection'])
+    task_state = fields.Selection(
+        selection=lambda self: self.env["project.task"].fields_get(allfields=["state"])["state"]["selection"]
+    )
 
-    @api.constrains('state_change', 'task_state')
+    @api.constrains("state_change", "task_state")
     def _check_task_state(self):
         for record in self:
             if record.state_change and not record.task_state:
-                raise UserError("The state to which the task should change cannot be empty if 'state change' is checked")
+                raise UserError(
+                    "The state to which the task should change cannot be empty if 'state change' is checked"
+                )
